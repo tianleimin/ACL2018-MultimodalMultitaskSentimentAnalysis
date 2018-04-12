c55e75d6448ddd50d53f2beadbb6be8ddf1481a8
Leimin Tian <s1219694@sms.ed.ac.uk>
2018-04-12 14:38:50 +0100

    Vocal S+I model

--- A_unimodal_int.py
+++ A_unimodal_int.py
@@ -1,5 +1,5 @@
 # Vocal unimodal multitask learning (valence+intensity) 
-# for ACL2018 Computational Modeling of Human Multimodal Language workshop paper
+# for ACL2018 Computational Modeling of Human Multimodal Language Workshop paper
 
 from __future__ import print_function
 import numpy as np
