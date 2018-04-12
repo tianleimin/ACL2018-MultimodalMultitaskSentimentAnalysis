9e23974f3583768144b20e72331372ab1301f25a
Leimin Tian <s1219694@sms.ed.ac.uk>
2018-04-12 14:53:07 +0100

    Visual S model

--- V_unimodal_uno.py
+++ V_unimodal_uno.py
@@ -1,5 +1,5 @@
-# Visual unimodal singletask learning for ACL2018 emotion challenge
-# Parameters following MOSI_2017 TFN paper
+# Visual unimodal singletask learning
+# for ACL2018 Computational Modeling of Human Multimodal Language Workshop paper
 
 from __future__ import print_function
 import numpy as np
