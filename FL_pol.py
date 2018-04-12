7ca0298c2524cb0aeed55c531c15ea585efe4ff9
Leimin Tian <s1219694@sms.ed.ac.uk>
2018-04-12 14:55:58 +0100

    FL S+P model

--- FL_pol.py
+++ FL_pol.py
@@ -1,4 +1,4 @@
-# FL multimodal multitask learning (valence+polarity) learning
+# FL multimodal multitask learning (valence+polarity)
 # for ACL2018 Computational Modeling of Human Multimodal Language Workshop paper
 
 from __future__ import print_function
