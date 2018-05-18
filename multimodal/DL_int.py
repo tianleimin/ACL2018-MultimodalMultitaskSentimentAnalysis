7bc67c0b7485d7cfaf67f8dba56c4da17430b5cd
Leimin Tian <leimin.tian@monash.edu>
2018-05-18 14:39:18 +0200

    Update DL_int.py

--- multimodal/DL_int.py
+++ multimodal/DL_int.py
@@ -1,4 +1,4 @@
-# DL multimodal multitask learning (valence+intensity)
+# Late Fusion multimodal multitask learning (valence+intensity)
 # for ACL2018 Computational Modeling of Human Multimodal Language Workshop paper
 
 from __future__ import print_function
