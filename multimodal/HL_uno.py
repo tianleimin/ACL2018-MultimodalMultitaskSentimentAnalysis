62d0176767391f3c024b6a0c38c21069f06c1280
Leimin Tian <s1219694@sms.ed.ac.uk>
2018-05-24 15:07:14 +0100

    Update HL_uno.py

--- multimodal/HL_uno.py
+++ multimodal/HL_uno.py
@@ -20,7 +20,7 @@ warnings.filterwarnings("ignore")
 # save outputs to a log file in case there is a broken pipe
 import sys
 idlestdout = sys.stdout
-logger = open("/exports/csce/datastore/inf/groups/eddie_inf_hcrc_cstr_students/s1219694/ACL2018/prediction/output_HL_uno.txt", "w")
+logger = open("prediction/output_HL_uno.txt", "w")
 sys.stdout = logger
 
 # custom evaluation metrics
@@ -197,7 +197,7 @@ print('Valence Test mae:', tst_mae_emo)
 
 # output predictions
 np.set_printoptions(threshold=np.nan)
-tst_pred_file = "/exports/csce/datastore/inf/groups/eddie_inf_hcrc_cstr_students/s1219694/ACL2018/prediction/pred_HL_uno.txt"
+tst_pred_file = "prediction/pred_HL_uno.txt"
 print('Printing predictions...')
 tst_pred = HL_model.predict([x_A_test, x_V_test, x_T_test])
 tst_df = pd.DataFrame(tst_pred)
