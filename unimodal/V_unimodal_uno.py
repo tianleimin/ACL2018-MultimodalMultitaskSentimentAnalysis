5e5733ae5c06b28a190dd2496818387ec648e5a7
Leimin Tian <s1219694@sms.ed.ac.uk>
2018-05-24 14:59:17 +0100

    Update V_unimodal_uno.py

--- unimodal/V_unimodal_uno.py
+++ unimodal/V_unimodal_uno.py
@@ -20,7 +20,7 @@ warnings.filterwarnings("ignore")
 # save outputs to a log file in case there is a broken pipe
 import sys
 idlestdout = sys.stdout
-logger = open("/exports/csce/datastore/inf/groups/eddie_inf_hcrc_cstr_students/s1219694/ACL2018/prediction/output_V_unimodal_uno.txt", "w")
+logger = open("prediction/output_V_unimodal_uno.txt", "w")
 sys.stdout = logger
 
 # custom evaluation metrics
@@ -163,16 +163,8 @@ print('Valence Test mae:', tst_mae_emo)
 
 # output predictions
 np.set_printoptions(threshold=np.nan)
-tst_pred_file = "/exports/csce/datastore/inf/groups/eddie_inf_hcrc_cstr_students/s1219694/ACL2018/prediction/pred_V_unimodal_uno.txt"
+tst_pred_file = "prediction/pred_V_unimodal_uno.txt"
 #print('Printing predictions...')
-tst_pred = model.predict(x_test)
-tst_df = pd.DataFrame(tst_pred)
-tst_df.to_csv(tst_pred_file, index=False, header=False)
-
-# output predictions
-np.set_printoptions(threshold=np.nan)
-tst_pred_file = "/exports/csce/datastore/inf/groups/eddie_inf_hcrc_cstr_students/s1219694/ACL2018/prediction/pred_V_unimodal_uno.txt"
-print('Printing predictions...')
 tst_pred = model.predict(x_test)
 tst_df = pd.DataFrame(tst_pred)
 tst_df.to_csv(tst_pred_file, index=False, header=False)
